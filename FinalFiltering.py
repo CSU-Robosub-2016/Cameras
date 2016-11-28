7a7fa5c5ea354c2def4c6c80729bfe39b2f3d276
bwgonzal <bwgonzal@rams.colostate.edu>
2016-11-27 22:08:43 -0700

    Update FinalFiltering.py

--- FinalFiltering.py
+++ FinalFiltering.py
@@ -2,20 +2,15 @@ import numpy as np
 import cv2
 
 #Last edited by Brett on 11/27/16
+
 #Read in original Image
 im = cv2.imread("C:/Users/Brett/Pictures/opencvv2.jpg")
+
 #Blur to remove noise
 im = cv2.medianBlur(im,5)
-#Convert to HSV to find only orange
-hsv_img = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-#Set min / max values to find only orange
-COLOR_MIN = np.array([5, 50, 50],np.uint8)
-COLOR_MAX = np.array([15, 255, 255],np.uint8)
 
-frame_threshed = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX)
-imgray = frame_threshed
 #Threshold filter to find contours
-ret,thresh = cv2.threshold(frame_threshed,153,255,0)
+ret,thresh = cv2.threshold(im,153,255,0)
 #Find Contours
 q, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)\
 
@@ -23,16 +18,18 @@ q, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_
 areas = [cv2.contourArea(c) for c in contours]
 max_index = np.argmax(areas)
 cnt=contours[max_index]
-#Create Rectangle around largest contour
+
+#Create Rectangle around largest contour (ROI)
 x,y,w,h = cv2.boundingRect(cnt)
 new= cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+
 #Compare region of interest to original image, set everything outside the ROI to zero
 mask = np.zeros(new.shape,np.uint8)
 mask[y:y+h,x:x+w] = new[y:y+h,x:x+w]
 out = cv2.bitwise_and(new, mask)
 
+#Filter for Max RGB values
 b,g,r = cv2.split(out)
-#FIlter for Max RGB values
 M = np.maximum(np.maximum(r, g), b)
 r[r < M] = 0
 g[g < M] = 0
@@ -52,7 +49,7 @@ mask = cv2.inRange(image2, lower, upper )
 output = cv2.bitwise_and(image2, image2, mask = mask)
 
 
-#SHow output compared to original
+#Show output compared to original
 cv2.imshow("Original", im)
 cv2.imshow("Final", output)
 cv2.waitKey()
